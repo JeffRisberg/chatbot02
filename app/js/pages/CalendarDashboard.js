b715bd8fd0935be257ba416dec9cc90e3c34cbe5
JeffRisberg <j.s.risberg@gmail.com>
2022-07-14 20:14:51 -0700

    updated calendar to have popup modal

--- app/js/pages/CalendarDashboard.js
+++ app/js/pages/CalendarDashboard.js
@@ -5,6 +5,8 @@ import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
+import TaskModal from '../Components/TaskModal/TaskModal';
+
 import './CalendarDashboard.css';
 
 // eslint-disable-next-line no-unused-vars
@@ -17,17 +19,35 @@ function CalendarDashboard(props) {
 
   useEffect(() => {
     (async () => {
-      const result = await axios('http://localhost:5000/api/weekly_tasks/' + user_id);
       const events = [];
+      var result = await axios('http://localhost:5000/api/weekly_tasks/' + user_id + '?all=1');
 
       result.data.forEach((weekly_task) => {
         const event = {};
 
-        const start = weekly_task.Monday;
-        const end = event.end;
+        const start = weekly_task.start;
+        const end = weekly_task.end;
 
         event.start = start.replace('T00:00:00', '');
         event.end = end.replace('T00:00:00', '');
+        event.title = 'Weekly ' + weekly_task.name;
+        event.color = weekly_task.done === 1 ? '#8F8' : '#F88';
+
+        events.push(event);
+      });
+
+      result = await axios('http://localhost:5000/api/daily_tasks/' + user_id + '?all=1');
+
+      result.data.forEach((daily_task) => {
+        const event = {};
+
+        const start = daily_task.start;
+        const end = daily_task.end;
+
+        event.start = start.replace('T00:00:00', '');
+        event.end = end.replace('T00:00:00', '');
+        event.title = daily_task.name;
+        event.color = daily_task.done === 1 ? '#8F8' : '#F88';
 
         events.push(event);
       });
@@ -43,6 +63,12 @@ function CalendarDashboard(props) {
         {eventInfo.event.title}
       </>
     )
+  }
+
+  function handleEventClick(clickInfo) {
+    if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+      clickInfo.event.remove() // will render immediately. will call handleEventRemove
+    }
   }
 
   console.log(data);
@@ -66,7 +92,7 @@ function CalendarDashboard(props) {
         //select={handleDateSelect}
         events={data}
         eventContent={renderEventContent} // custom render function
-        //eventClick={this.handleEventClick}
+        eventClick={handleEventClick}
         //eventAdd={this.handleEventAdd}
         //eventChange={this.handleEventChange} // called for drag-n-drop/resize
         //eventRemove={this.handleEventRemove}
