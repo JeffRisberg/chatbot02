466409423fb1726ab80649f0deec37f0715caaa2
JeffRisberg <j.s.risberg@gmail.com>
2022-07-16 18:54:08 -0700

    connect home screen to redux

--- app/js/pages/Home.js
+++ app/js/pages/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import TopMenu from '../components/TopMenu/TopMenu';
 import {connect} from 'react-redux';
+import TopMenu from '../components/TopMenu/TopMenu';
+import axios from 'axios';
+import {set_screen} from "../actions/screen";
 import './Home.css';
 
 function Home(props) {
@@ -8,10 +10,43 @@ function Home(props) {
 
   const firstName = user !== null ? user.first_name : '';
 
+  function onMonthly() {
+    const my_name = 'monthly';
+    axios.post('http://localhost:5000/change_screen/' + my_name, null, {
+      withCredentials: true,
+    })
+      .then((resp) => {
+        props.set_screen(my_name, resp.data);
+      });
+  }
+
+  function onWeekly() {
+    const my_name = 'weekly';
+
+    axios.post('http://localhost:5000/change_screen/' + my_name, null, {
+      withCredentials: true,
+    })
+      .then((resp) => {
+        props.set_screen(my_name, resp.data);
+      });
+  }
+
   return (
     <div className='home-container'>
-      <TopMenu />
-      <div style={{height: '600px', color: 'white', display: 'grid', alignItems: 'center', justifyContent: 'center'}}>
+      <div style={{height: '45px'}}>
+        <div style={{float: 'left'}}>
+          <TopMenu/>
+        </div>
+        <div style={{float: 'right'}}>
+          <button onClick={onWeekly} style={{background: 'none', borderWidth: 0}}>
+            <span style={{color: 'white'}}>This Week</span>
+          </button>
+          <button onClick={onMonthly} style={{background: 'none', borderWidth: 0}}>
+            <span style={{color: 'white'}}>This Month</span>
+          </button>
+        </div>
+      </div>
+      <div style={{height: '550px', color: 'white', display: 'grid', alignItems: 'center', justifyContent: 'center'}}>
         <h1>Hi, {firstName}</h1>
       </div>
     </div>
@@ -24,5 +59,5 @@ const mapStateToProps = (state) => ({
 
 export default connect(
   mapStateToProps,
-  null,
+  {set_screen}
 )(Home);
