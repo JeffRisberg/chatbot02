8982b94fa7f32e9ad42e160adb7345f74b2e3b16
JeffRisberg <j.s.risberg@gmail.com>
2022-08-14 22:09:45 -0700

    updated to match demo

---
+++ app/js/reducers/event.js
@@ -0,0 +1,31 @@
+import {handleActions} from 'redux-actions';
+import {ActionTypes as types} from '../constants';
+
+export default handleActions({
+    [types.SET_EVENTS]: (state, action) => {
+      state.events = action.events;
+      return {
+        events: action.events
+      };
+    },
+    [types.ADD_EVENT]: (state, action) => {
+      return {
+        events: [...state.events, action.event]
+      }
+    },
+    [types.UPDATE_EVENT]: (state, action) => {
+      const index = state.events.findIndex(event => event.id === action.event.id)
+      const newEvents = [...state.events]
+      if (index >= 0) {
+        newEvents[index] = action.event
+      }
+      return {
+        events: newEvents
+      };
+    },
+    [types.DELETE_EVENT]: (state, action) => {
+      return {
+        events: state.events.filter(event => event.id !== action.event.id)
+      };
+    }
+  }, {events: []});
--- app/js/reducers/index.js
+++ app/js/reducers/index.js
@@ -2,9 +2,11 @@ import {combineReducers} from 'redux';
 import user from './user';
 import content from './content';
 import screen from './screen';
+import event from './event';
 
 export default combineReducers({
   user,
   content,
-  screen
+  screen,
+  event
 });
